f93a6055e49137543f877a6e0853d0fe92597e2d
Arif İskilip <71379630+arifiskilip@users.noreply.github.com>
2023-03-16 00:20:08 +0300

    16.03.2023
    Added Option Component

---
+++ WebUI/ViewComponents/Default/Home/_Options.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.Default.Home
+{
+    public class _Options : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
