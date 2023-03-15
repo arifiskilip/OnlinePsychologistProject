a206bffc9d8c6c094e89a2da7ec7a2a0af5df71e
Reşit <resitcgdm@gmail.com>
2023-03-16 00:13:43 +0300

    16.03.2023

---
+++ WebUI/ViewComponents/Default/Home/_SearchDestination.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.Default.Home
+{
+    public class _SearchDestination : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebUI/ViewComponents/Default/Home/_Statistics.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.Default.Home
+{
+    public class _Statistics : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
